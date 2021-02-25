5f987c97c28a7588834fdbc237fd70582c14b262
Carson Wilber <cdw37@students.uwf.edu>
2021-02-25 13:45:20 -0600

    Redux harvester with credentials and UUIDv4 name generation.

--- tweepy_utils.py
+++ tweepy_utils.py
@@ -1,35 +1,45 @@
-from utils.configuration import Configurable
-from utils.logging import Loggable
+from utils.configuration import Configurable, InstanceConfigurable
+from utils.log import Logging
+from utils.credentials import Credential, Credentials
+import utils.validation as validation
+from utils.uuid import uuidv4
 
 import tweepy
 
 
-class TwitterHarvester(Configurable, Loggable):
-    pass
+class TwitterHarvester(Logging, Configurable):
+    _harvesters = {}
 
+    def __init__(self, name: str = None):
+        if name is not None:
+            validation.validate_string(
+                name, 'harvester name', min_len=1, max_len=36)
 
-_configurable = {
-    'default': {
+            if name in TwitterHarvester._harvesters:
+                raise Exception(
+                    "Harvester names must be globally unique; cannot instantiate another harvester named '%s'." % name)
+        else:
+            name = uuidv4()
+            while name in TwitterHarvester._harvesters:
+                name = uuidv4()
 
-    },
-    'auth': {
-        'key': {
-            'type': str,
-            'match': '<some-regex>'
-        },
-        'secret': {
-            'type': str,
-            'match': '<some-regex>'
-        }
-    },
-    'api': {
-        'key': {
-            'type': str,
-            'match': '<some-regex>'
-        },
-        'secret': {
-            'type': str,
-            'match': '<some-regex>'
-        }
-    }
-}
+        super(InstanceConfigurable, self).__init__()
+
+        self.name = name
+        TwitterHarvester._harvesters[name] = self
+
+        self._api_init = False
+
+    def init(self, cred: Credential):
+        if self._api_init:
+            raise Exception(
+                "Harvester API access already initialized; if attempting to change credentials, please create a new harvester.")
+
+        if not cred.format == Credentials.OAuthConsumer:
+            raise Exception(
+                "Harvester only accepts OAuthConsumer type credentials.")
+
+        auth = tweepy.AppAuthHandler(
+            cred.details['key'], cred.details['secret'])
+        self.api = tweepy.API(auth_handler=auth, wait_on_rate_limit=True)
+        self._api_init = True
