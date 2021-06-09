710042a5c315aaf2bd5c5fbe5bbe6718e3b53e98
Carson Wilber <cdw37@students.uwf.edu>
2021-06-09 15:19:36 -0500

    Structure scraper for t-SNE modularity and hashtag collection.

--- ssdc-scraper.py
+++ ssdc-scraper.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Scrape data for the SSDC project.")  # noqa
     parser.add_argument("--source", type=str, required=True, help="Load this source file.")  # noqa
     parser.add_argument("--select", type=str, required=True, help="Select this reference point for each source item.")  # noqa
-    parser.add_argument("--action", choices=["timeline"], required=True, help="Perform this action on the given sources.")  # noqa
+    parser.add_argument("--action", choices=["timeline", "hashtag"], required=True, help="Perform this action on the given sources.")  # noqa
     parser.add_argument("--filter", type=str, help="Filter options to apply to source files.")  # noqa
     parser.add_argument("--credentials", type=str, default="./credentials.ini", help="Credential configuration file to use.")  # noqa
     parser.add_argument("--credential-name", type=str, default="twitter-ssdc-consumer", help="Credential configuration file to use.")  # noqa
--- tsne.py
+++ tsne.py
@@ -4,62 +4,65 @@ import gensim
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 
-skip = False
-if len(sys.argv) > 1 and sys.argv[1] == 'skip':
-    skip = True
+MAXIMUM = int(os.environ['TSNE_MAXIMUM']) if 'TSNE_MAXIMUM' in os.environ else 10000  # noqa
+PLOT_MAXIMUM = int(os.environ['PLOT_MAXIMUM']) if 'PLOT_MAXIMUM' in os.environ else 100  # noqa
 
-MAXIMUM = int(os.environ['TSNE_MAXIMUM']) if 'TSNE_MAXIMUM' in os.environ else 10000
-PLOT_MAXIMUM = int(os.environ['PLOT_MAXIMUM']) if 'PLOT_MAXIMUM' in os.environ else 100
+model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin.gz', binary=True)  # noqa
 
-imported = False
-labels = []
-xs = []
-ys = []
 
-if os.path.exists('./tsne.csv') and not skip:
-    print("Loading saved data!")
-    with open('./tsne.csv', 'r') as f:
-        for line in f.readlines():
-            components = line.split(',')
-            labels.append(','.join(components[:-2]))
-            xs.append(float(components[-2]))
-            ys.append(float(components[-1]))
-    imported = True
-    print("Loaded saved data!")
-else:
-    # Load Google's pre-trained Word2Vec model.
-    print("Loading model!")
-    model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin.gz', binary=True)  
-    print("Loaded model!")
+def calculate_embeddings(phrase):
+    if isinstance(phrase, str):
+        phrase = phrase.split()
+    elif not isinstance(phrase, list):
+        raise Exception(
+            "Must provide a string or list of strings to calculate an embedding.")
 
-    tokens = []
+    embeddings = []
 
-    i = 0
-    for word in model.wv.vocab:
-        tokens.append(model[word])
-        labels.append(word)
-        i += 1
-        if i == MAXIMUM: break
-    
-    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
-    new_values = tsne_model.fit_transform(tokens)
+    for word in phrase[:min(len(phrase), MAXIMUM)]:
+        if word in model.wv.vocab:
+            embeddings.append(model[word])
 
-    for value in new_values:
-        xs.append(value[0])
-        ys.append(value[1])
+    try:
+        return calculate_average(embedding)
+    except:
+        raise Exception(
+            "Failed to find enough valid tokens to parse as an embedding.")
+
+
+def calculate_average(embeddings):
+    if len(embeddings) < 2:
+        raise Exception("Must provide at least two embeddings to average.")
+
+    result = embeddings[0]
+    for embedding in embeddings[1:min(len(embeddings), MAXIMUM)]:
+        result = result + embedding
+
+    return result / i
+
+
+tsne_model = TSNE(perplexity=40, n_components=2,
+                  init='pca', n_iter=2500, random_state=23)
+new_values = tsne_model.fit_transform(tokens)
+
+for value in new_values:
+    xs.append(value[0])
+    ys.append(value[1])
 
 if not imported:
     with open('./tsne.csv', 'w+') as f:
         for i in range(len(xs)):
             f.write('%s,%s,%s\n' % (labels[i], xs[i], ys[i]))
 
-plt.figure(figsize=(16, 16)) 
-for i in range(min(PLOT_MAXIMUM, len(xs))):
-    plt.scatter(xs[i],ys[i])
-    plt.annotate(labels[i],
-                 xy=(xs[i], ys[i]),
-                 xytext=(5, 2),
-                 textcoords='offset points',
-                 ha='right',
-                 va='bottom')
-plt.show()
+
+def show_plot(xys):
+    plt.figure(figsize=(16, 16))
+    for i in range(min(PLOT_MAXIMUM, len(xs))):
+        plt.scatter(xs[i], ys[i])
+        plt.annotate(labels[i],
+                     xy=(xs[i], ys[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
